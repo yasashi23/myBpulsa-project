ea0847aaee244d97ae498a51850e28716bedb761
yasashibp <yasashibp@pop-os.localdomain>
2023-09-06 22:48:46 +0700

    update 6 sep 22:48

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,24 @@ import './App.css'
 import Checkout from './pages/Checkout'
 import PilihPaket from './pages/PilihPaket'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import React,{ useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import ErrorWeb from './pages/ErrorWeb'
 import OperatorSquare from './component/OperatorSquare'
+import Cookies from 'js-cookie'
 
 
 
 
 export default function App() {
-  const [konfirmasiPage,setKonfirmasiPage] = useState({data:{},aman:false,link:'/checkout/for-udin'})
-  const [theData, setTheData] = useState({})
 
+    useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+    console.log("User Agent:HJAHHAHAHHAHJHSDJKSHAJKHDAJK", userAgent);
+  }, []);
+
+  const [konfirmasiPage,setKonfirmasiPage] = useState({data:{},aman:false,link:''})
+  const [theData, setTheData] = useState({})
+  const nilaiCookie = Cookies.get('linkPembayaran')
 
 
   const [cekApi, setCekApi ] = useState(true)
@@ -21,13 +28,20 @@ export default function App() {
     setCekApi(e)
   }
 
+  const centerSet = {
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center'
+  }
+
   return(
-    <div>
-      {console.log(konfirmasiPage.link)}
+    <div style={{...centerSet}}>
+ 
       <Router>
         <Routes>
           <Route path='/a' element={<PilihPaket koonfirmasi={konfirmasiPage} setKonfirmasiPage={setKonfirmasiPage} setData={setTheData} cekApi={cekApi} setApi={ErrPage}/>}/>
-          <Route path={'/gas'} element={<Checkout konfirmasiPage = {konfirmasiPage} data={theData}/>}/>
+          <Route path={nilaiCookie} element={<Checkout konfirmasiPage = {konfirmasiPage} data={theData}/>}/>
+
           <Route path='/*' element={<ErrorWeb cekApi={cekApi}/>}/>
 
         {/* UNTUK DEVELOP SEMENTARA */}
@@ -37,7 +51,7 @@ export default function App() {
         </Routes>
         {/* {cekApi ? (<Navigate to='/'/>):(<Navigate to='/error'/>)}   */}
       </Router>
-    {console.log("KONFIRMASI RESULT",cekApi)}
     </div>
+  
   )
   }
